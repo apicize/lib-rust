937025b647075605f296e83afec37abeef5f32fe
Jason Terando <jason.t@digitalroominc.com>
2024-11-05 17:17:20 -0600

    Update Cargo build to run npm install

--- build.rs
+++ build.rs
@@ -1,17 +1,36 @@
-use std::process::Command;
+use std::{path::Path, process::Command};
 
 fn main() {
-    let result = Command::new("npm")
+    let node_modules = Path::new("./src/static/src/node_modules");
+    if !node_modules.exists() {
+        let result_install = Command::new("npm")
+            .current_dir("./src/static/src")
+            .args(&["install"])
+            .status();
+
+        match result_install {
+            Ok(status) => {
+                if !status.success() {
+                    panic!("Unable to initialize NodeJS");
+                }
+            }
+            Err(err) => {
+                panic!("Error initilalizing NodeJS: {}", err);
+            }
+        }
+    }
+
+    let result_build = Command::new("npm")
         .current_dir("./src/static/src")
         .args(&["run", "build"])
         .status();
 
-    match result {
+    match result_build {
         Ok(status) => {
-            if ! status.success() {
+            if !status.success() {
                 panic!("Unable to build framework JavaScript");
             }
-        },
+        }
         Err(err) => {
             panic!("Error building framework JavaScript: {}", err);
         }
