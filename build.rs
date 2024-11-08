0b912d198e796264f6dae09fb9b269180a4bad8e
Jason Terando <jason.t@digitalroominc.com>
2024-11-07 19:47:50 -0600

    Move framwork.min.js out of Cargo build, copy pre-built file instead

--- build.rs
+++ build.rs
@@ -4,17 +4,14 @@ use std::{
 };
 
 fn main() {
-    
+    let out = std::env::var("OUT_DIR").unwrap();
     let source_path = Path::new("test-framework").join("dist").join("framework.min.js");
-    let dest_path = Path::new(std::env::var("OUT_DIR").unwrap().as_str()).join("test-framework");
-    
-    println!("Copy framework file {} to {}", source_path.to_str().unwrap(), dest_path.to_str().unwrap());
+    let dest_path = Path::new(&out);
+    let dest_file_name = Path::new(&dest_path).join("framework.min.js");
+
+    // println!("Copy framework file {} to {}", source_path.to_str().unwrap(), dest_file_name.to_str().unwrap());
     if !Path::exists(&dest_path) {
         create_dir(&dest_path).unwrap();
     }
-    copy(
-        source_path,
-        Path::new(&dest_path).join("framework.min.js")
-    )
-    .unwrap();
+    copy(&source_path, &dest_file_name).unwrap();
 }
